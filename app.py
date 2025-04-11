c76939c9fbd1bd7ba4822b6a91562be0dffc7a8d
Shreyas Nikam <158344330+shreyas-nikam@users.noreply.github.com>
2025-04-11 11:33:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
-
 import streamlit as st
+from application_pages import representations_of_market, identifying_arbitrage_cycles, live_arbitrage_identification, practical_challenges
 
-st.set_page_config(page_title="QuCreate Streamlit Lab", layout="wide")
-st.sidebar.image("assets/images/company_logo.jpg")
+st.set_page_config(page_title="QuLab", layout="wide")
+st.sidebar.image("https://www.quantuniversity.com/assets/img/logo5.jpg")
 st.sidebar.divider()
-st.title("QuCreate Streamlit Lab")
+st.title("QuLab")
 st.divider()
 
-# Code goes here
+# Navigation
+page = st.sidebar.selectbox(
+    label="Navigation",
+    options=[
+        "Representations of a Market",
+        "Identifying Arbitrage Cycles",
+        "Live Arbitrage Identification",
+        "Practical Challenges of Arbitrage Trading",
+    ],
+)
+
+if page == "Representations of a Market":
+    representations_of_market.app()
+elif page == "Identifying Arbitrage Cycles":
+    identifying_arbitrage_cycles.app()
+elif page == "Live Arbitrage Identification":
+    live_arbitrage_identification.app()
+elif page == "Practical Challenges of Arbitrage Trading":
+    practical_challenges.app()
 
 st.divider()
 st.write("© 2025 QuantUniversity. All Rights Reserved.")
-st.caption("The purpose of this demonstration is solely for educational use and illustration. "
-           "To access the full legal documentation, please visit this link. Any reproduction of this demonstration "
-           "requires prior written consent from QuantUniversity.")
+st.caption(
+    "The purpose of this demonstration is solely for educational use and illustration. "
+    "Any reproduction of this demonstration "
+    "requires prior written consent from QuantUniversity."
+)
