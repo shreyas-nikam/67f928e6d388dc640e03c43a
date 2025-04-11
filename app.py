be06f41d2a56b03eaa5839be74c0fa7db011b2d7
Shreyas Nikam <158344330+shreyas-nikam@users.noreply.github.com>
2025-04-11 12:58:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,31 @@
+
 import streamlit as st
 
-# Set up page configuration and header
 st.set_page_config(page_title="QuLab", layout="wide")
 st.sidebar.image("https://www.quantuniversity.com/assets/img/logo5.jpg")
 st.sidebar.divider()
 st.title("QuLab")
 st.divider()
 
-# Navigation Sidebar
-page = st.sidebar.selectbox("Navigate", 
-    ["Market Representations", "Identifying Arbitrage Cycles", "Live Arbitrage Identification", "Practical Challenges of Arbitrage Trading"])
+# Your code goes here
+page = st.sidebar.selectbox(label="Navigation", options=["Representations of a Market", "Identifying Arbitrage Cycles", "Live Arbitrage Identification", "Practical Challenges of Arbitrage Trading"])
 
-if page == "Market Representations":
-    from pages import market_representations as mr
-    mr.app()
+if page == "Representations of a Market":
+    from application_pages.representations import run_representations
+    run_representations()
 elif page == "Identifying Arbitrage Cycles":
-    from pages import arbitrage_cycle as ac
-    ac.app()
+    from application_pages.arbitrage_cycles import run_arbitrage_cycles
+    run_arbitrage_cycles()
 elif page == "Live Arbitrage Identification":
-    from pages import live_arbitrage as la
-    la.app()
+    from application_pages.live_arbitrage import run_live_arbitrage
+    run_live_arbitrage()
 elif page == "Practical Challenges of Arbitrage Trading":
-    from pages import practical_challenges as pc
-    pc.app()
+    from application_pages.challenges import run_challenges
+    run_challenges()
+
 
 st.divider()
 st.write("© 2025 QuantUniversity. All Rights Reserved.")
 st.caption("The purpose of this demonstration is solely for educational use and illustration. "
-           "Any reproduction of this demonstration requires prior written consent from QuantUniversity.")
+           "Any reproduction of this demonstration "
+           "requires prior written consent from QuantUniversity.")
