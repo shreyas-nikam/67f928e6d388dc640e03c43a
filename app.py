fbfdcca682bbddc5c28a8937018af07e337c3e09
Shreyas Nikam <158344330+shreyas-nikam@users.noreply.github.com>
2025-04-11 11:34:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from application_pages import representations_of_market, identifying_arbitrage_cycles, live_arbitrage_identification, practical_challenges
 
 st.set_page_config(page_title="QuLab", layout="wide")
 st.sidebar.image("https://www.quantuniversity.com/assets/img/logo5.jpg")
@@ -7,30 +6,27 @@ st.sidebar.divider()
 st.title("QuLab")
 st.divider()
 
-# Navigation
-page = st.sidebar.selectbox(
-    label="Navigation",
-    options=[
-        "Representations of a Market",
-        "Identifying Arbitrage Cycles",
-        "Live Arbitrage Identification",
-        "Practical Challenges of Arbitrage Trading",
-    ],
-)
+# Navigation selectbox to choose pages
+page = st.sidebar.selectbox("Navigation", [
+    "Representations of a Market", 
+    "Identifying Arbitrage Cycles", 
+    "Live Arbitrage Identification", 
+    "Practical Challenges of Arbitrage Trading"
+])
 
 if page == "Representations of a Market":
-    representations_of_market.app()
+    from pages import market
+    market.app()
 elif page == "Identifying Arbitrage Cycles":
-    identifying_arbitrage_cycles.app()
+    from pages import arbitrage
+    arbitrage.app()
 elif page == "Live Arbitrage Identification":
-    live_arbitrage_identification.app()
+    from pages import live_arbitrage
+    live_arbitrage.app()
 elif page == "Practical Challenges of Arbitrage Trading":
-    practical_challenges.app()
+    from pages import challenges
+    challenges.app()
 
 st.divider()
 st.write("© 2025 QuantUniversity. All Rights Reserved.")
-st.caption(
-    "The purpose of this demonstration is solely for educational use and illustration. "
-    "Any reproduction of this demonstration "
-    "requires prior written consent from QuantUniversity."
-)
+st.caption("The purpose of this demonstration is solely for educational use and illustration. Any reproduction of this demonstration requires prior written consent from QuantUniversity.")
